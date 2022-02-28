d01e22253173beef79a0d3b61d421ecc9146e05b
ASUS <https://github.com/Sluckalex>
2022-02-28 22:24:29 +0300

    dekrement

---
+++ src/dekrement.java
@@ -0,0 +1,9 @@
+public class dekrement {
+    public static void main(String[] args) {
+        for (int i = 5; i < 6; i--) {
+            if (i > 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
