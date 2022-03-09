b5c017cf769ab3912b91d3aa709023c80d89399b
ASUS <https://github.com/Sluckalex>
2022-03-09 19:44:36 +0300

    while7

---
+++ src/com/company/work4.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class work4 {
+    public static void main(String[] args) {
+        int i = 7;
+        int j = 1;
+        while (j <15) {
+            System.out.println(i * j + " ");
+            j++;
+        }
+        System.out.println();
+    }
+}
