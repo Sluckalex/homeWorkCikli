1a8bae65c85f4ad648b93639cfdb6508d9987b34
ASUS <https://github.com/Sluckalex>
2022-03-09 19:55:35 +0300

    for6

---
+++ src/com/company/work6.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class work6 {
+    public static void main(String[] args) {
+        for (int j = 10; j < 21; j++) {
+            System.out.println(j * j);
+        }
+    }
+}
