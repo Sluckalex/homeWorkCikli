12a3e0e5880e41118b06dddff1478c2f9087468e
ASUS <https://github.com/Sluckalex>
2022-03-09 19:53:12 +0300

    for5

---
+++ src/com/company/work5.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class work5 {
+    public static void main(String[] args) {
+        int j=0;
+        for(int i=0; i<10;i++) {
+            System.out.println(j);
+            j-=5;
+        }
+    }
+}
