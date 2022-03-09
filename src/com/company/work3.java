f737826caecf588b7571248de562966934ae5578
ASUS <https://github.com/Sluckalex>
2022-03-09 19:41:12 +0300

    sumandscanner

---
+++ src/com/company/work3.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class work3 {
+    public static void main(String[] args) {
+        System.out.println("Введите любое целое положительное число : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i ++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
